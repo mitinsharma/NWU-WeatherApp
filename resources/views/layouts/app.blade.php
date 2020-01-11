78fafc06a87a496d245c24b249a4e7ca0e745c83
Mitin Sharma <Mitin.Sharma@marist.edu>
2020-01-11 12:09:11 -0500

    Weather UI added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" crossorigin="anonymous"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
         <!-- Styles -->
@@ -20,155 +20,54 @@
                 font-family: 'Montserrat', sans-serif;
                 //background: #112233;
             }
+            .weather
+            {
+                display: flex;
+                flex-flow: column wrap;
+                box-shadow: 0px 1px 10px 0px #cfcfcf;
+                overflow: hidden;
+            }
 
-            .weather-card {
-                margin: 60px auto;
-                height: 740px;
-                width: 450px;
-                background: #fff;
-                box-shadow: 0 1px 38px rgba(0, 0, 0, 0.15), 0 5px 12px rgba(0, 0, 0, 0.25);
-                overflow: hidden;
+            .weather .current
+            {
+                display: flex;
+                flex-flow: row wrap;
+                background-image: url("http://www.prepbootstrap.com/Content/images/shared/misc/london-view.png");
+                background-repeat: repeat-x;
+                color: white;
+                padding: 20px;
+                text-shadow: 1px 1px #F68D2E;
             }
-            .weather-card .top {
-                position: relative;
-                height: 570px;
-                width: 100%;
-                overflow: hidden;
-                background: url("https://s-media-cache-ak0.pinimg.com/564x/cf/1e/c4/cf1ec4b0c96e59657a46867a91bb0d1e.jpg") no-repeat;
-                background-size: cover;
-                background-position: center center;
+
+            .weather .current .info
+            {
+                display: flex;
+                flex-flow: column wrap;
+                justify-content: space-around;
+                flex-grow: 2;
+            }
+
+            .weather .current .info .city
+            {
+                font-size: 26px;
+            }
+
+            .weather .current .info .temp
+            {
+                font-size: 56px;
+            }
+
+            .weather .current .info .wind
+            {
+                font-size: 24px;
+            }
+
+            .weather .current .icon
+            {
                 text-align: center;
-            }
-            .weather-card .top .wrapper {
-                padding: 30px;
-                position: relative;
-                z-index: 1;
-            }
-            .weather-card .top .wrapper .mynav {
-                height: 20px;
-            }
-            .weather-card .top .wrapper .mynav .lnr {
-                color: #fff;
-                font-size: 20px;
-            }
-            .weather-card .top .wrapper .mynav .lnr-chevron-left {
-                display: inline-block;
-                float: left;
-            }
-            .weather-card .top .wrapper .mynav .lnr-cog {
-                display: inline-block;
-                float: right;
-            }
-            .weather-card .top .wrapper .heading {
-                margin-top: 20px;
-                font-size: 35px;
-                font-weight: 400;
-                color: #fff;
-            }
-            .weather-card .top .wrapper .location {
-                margin-top: 20px;
-                font-size: 21px;
-                font-weight: 400;
-                color: #fff;
-            }
-            .weather-card .top .wrapper .temp {
-                margin-top: 20px;
-            }
-            .weather-card .top .wrapper .temp a {
-                text-decoration: none;
-                color: #fff;
-            }
-            .weather-card .top .wrapper .temp a .temp-type {
-                font-size: 85px;
-            }
-            .weather-card .top .wrapper .temp .temp-value {
-                display: inline-block;
-                font-size: 85px;
-                font-weight: 600;
-                color: #fff;
-            }
-            .weather-card .top .wrapper .temp .deg {
-                display: inline-block;
-                font-size: 35px;
-                font-weight: 600;
-                color: #fff;
-                vertical-align: top;
-                margin-top: 10px;
-            }
-            .weather-card .top:after {
-                content: "";
-                height: 100%;
-                width: 100%;
-                display: block;
-                position: absolute;
-                top: 0;
-                left: 0;
-                background: rgba(0, 0, 0, 0.5);
-            }
-            .weather-card .bottom {
-                padding: 0 30px;
-                background: #fff;
-            }
-            .weather-card .bottom .wrapper .forecast {
-                overflow: hidden;
-                margin: 0;
-                font-size: 0;
-                padding: 0;
-                padding-top: 20px;
-                max-height: 155px;
-            }
-            .weather-card .bottom .wrapper .forecast a {
-                text-decoration: none;
-                color: #000;
-            }
-            .weather-card .bottom .wrapper .forecast .go-up {
-                text-align: center;
-                display: block;
-                font-size: 25px;
-                margin-bottom: 10px;
-            }
-            .weather-card .bottom .wrapper .forecast li {
-                display: block;
-                font-size: 25px;
-                font-weight: 400;
-                color: rgba(0, 0, 0, 0.25);
-                line-height: 1em;
-                margin-bottom: 30px;
-            }
-            .weather-card .bottom .wrapper .forecast li .date {
-                display: inline-block;
-            }
-            .weather-card .bottom .wrapper .forecast li .condition {
-                display: inline-block;
-                vertical-align: middle;
-                float: right;
-                font-size: 25px;
-            }
-            .weather-card .bottom .wrapper .forecast li .condition .temp {
-                display: inline-block;
-                vertical-align: top;
-                font-family: 'Montserrat', sans-serif;
-                font-size: 20px;
-                font-weight: 400;
-                padding-top: 2px;
-            }
-            .weather-card .bottom .wrapper .forecast li .condition .temp .deg {
-                display: inline-block;
-                font-size: 10px;
-                font-weight: 600;
-                margin-left: 3px;
-                vertical-align: top;
-            }
-            .weather-card .bottom .wrapper .forecast li .condition .temp .temp-type {
-                font-size: 20px;
-            }
-            .weather-card .bottom .wrapper .forecast li.active {
-                color: rgba(0, 0, 0, 0.8);
-            }
-            .weather-card.rain .top {
-                background: url("http://img.freepik.com/free-vector/girl-with-umbrella_1325-5.jpg?size=338&ext=jpg") no-repeat;
-                background-size: cover;
-                background-position: center center;
+                font-size: 64px;
+                flex-grow: 1;
+                text-shadow: 1px 1px #F68D2E;
             }
 
         </style>
--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.app')
 @section('content')
-    <h2></h2>
-    <p>Temperature: </p>
-
-
     <div class="container">
+        <br/>
+        <h2 class="text-sm-center">NWU Weather Information:</h2>
+        <br/>
+        <br/>
         <div class="row">
-            <div class="col">
-                <div class="weather-card">
-                    <div class="top">
-                        <div class="wrapper">
-                            <h1 class="heading">{{$res['location']}}</h1>
-                            <h3 class="location"></h3>
-                            <p class="temp">
-                                <span class="temp-value">{{ ($res['temperature'])}}</span>
-                                <span class="deg">0</span>
-                                <a href="javascript:;"><span class="temp-type">F</span></a>
-                            </p>
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="weather">
+                    <div class="current">
+                        <div class="info">
+                            <div>&nbsp;</div>
+                            <div class="city"><small>CITY:</small> {{$res['location']}}</div>
+                            <div class="temp">{{ ($res['temperature'])}}&deg; <small>F</small></div>
+                            <div class="wind"><small><small>WIND:</small></small> 22 km/h</div>
+                            <div>&nbsp;</div>
+                        </div>
+                        <div class="icon">
+                            @foreach($res['forecast'] as $forecast)
+                                <img src="http://openweathermap.org/img/wn/{{$forecast['icon']}}@2x.png" title="{{$forecast['description']}}">
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-4"></div>
         </div>
     </div>
-
 @endsection
